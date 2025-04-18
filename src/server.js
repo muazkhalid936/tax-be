bf1d0516284f4143ce838aeeeae0d2c1b0308acc
Muaz Khalid <itzmajo786@gmail.com>
2025-04-18 14:28:40 +0500

    Api Error

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 // CORS configuration
 const allowedOrigins = [
   "http://localhost:3000", // Your local development
-  "https://your-production-frontend.com" // Your production frontend
+  "https://taxtilla.vercel.app/" // Your production frontend
 ];
 
 const corsOptions = {
